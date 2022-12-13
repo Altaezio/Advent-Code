05fa60bea9b74f5e86b49fc95cbef300b37c66d1
Altaezio <v.robert.jaunet@gmail.com>
2022-12-13 16:37:30 +0100

    Begin D13

--- 2022/Solution13.1.py
+++ 2022/Solution13.1.py
@@ -1,5 +1,22 @@
 data = open("test.txt", "r")
 
+def toList(packet):
+    totalList = []
+    depthStack = [0]
+    currentList = totalList
+    for carac in packet[1:]:
+        match carac:
+            case '[':
+                currentList.append([])
+                depthStack.append(0)
+                currentList = currentList[-1]
+            case ']':
+                depthStack.pop()
+                currentList = totalList
+                for index in depthStack:
+                    currentList = currentList[index]
+            case ','
+
 packets = []
 
 for lineIndex, line in enumerate(data):
@@ -7,7 +24,8 @@ for lineIndex, line in enumerate(data):
     if len(line) == 0:
         packets = []
         continue
-    packets.append(line)
+    packets.append(list(line))
     if len(packets) == 2:
-        
+        list1 = toList(packets[0])
+        list2 = toList(packets[1])
     
