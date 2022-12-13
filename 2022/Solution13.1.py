eefe4f6a20f1c542fd649aaebf2468caa6ef87ae
Altaezio <v.robert.jaunet@gmail.com>
2022-12-13 17:13:45 +0100

    D13 wip

--- 2022/Solution13.1.py
+++ 2022/Solution13.1.py
@@ -1,23 +1,45 @@
 data = open("test.txt", "r")
 
 def toList(packet):
+    #print("traitement de :",packet)
     totalList = []
     depthStack = [0]
     currentList = totalList
+    numberStr = ''
     for carac in packet[1:]:
+        #print("carac :",carac)
         match carac:
             case '[':
                 currentList.append([])
                 depthStack.append(0)
                 currentList = currentList[-1]
             case ']':
-                depthStack.pop()
+                if numberStr != '':
+                    currentList.append(int(numberStr))
+                numberStr = ''
                 currentList = totalList
-                for index in depthStack:
+                depthStack.pop()
+                for index in depthStack[:-1]:
                     currentList = currentList[index]
-            case ','
+                if len(depthStack) > 0:
+                    depthStack[-1] += 1
+            case ',':
+                if numberStr != '':
+                    currentList.append(int(numberStr))
+                    numberStr = ''
+                depthStack[-1] += 1
+            case _:
+                numberStr += carac
+    return totalList
+                
+def areInOrder(list1, list2):
+    if len(list1) > len(list2):
+        return False
+    print("are they in order ? :",list1, "and",list2)
+    
 
 packets = []
+indexSum = 0
 
 for lineIndex, line in enumerate(data):
     line = line.rstrip()
@@ -26,6 +48,11 @@ for lineIndex, line in enumerate(data):
         continue
     packets.append(list(line))
     if len(packets) == 2:
+        #print(packets)
         list1 = toList(packets[0])
         list2 = toList(packets[1])
-    
+
+        if areInOrder(list1, list2):
+            indexSum += 2 * lineIndex - 1
+
+print(indexSum)    
